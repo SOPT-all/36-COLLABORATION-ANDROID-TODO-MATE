4d1cd6638eb83dbc9cddf335da057eef6e6db73f
Daljyeong <jiyoungm1022@gmail.com>
2025-05-18 22:51:00 +0900

    [UI] #13 DayItem 컴포넌트 구현 완료

---
+++ app/src/main/java/com/example/myapplication/presentation/home/calender/Weekday.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.presentation.home.calender
+
+enum class Weekday(val day: String) {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+}
