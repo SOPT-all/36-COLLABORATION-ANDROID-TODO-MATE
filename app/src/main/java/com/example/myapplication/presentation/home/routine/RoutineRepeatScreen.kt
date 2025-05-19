d0ebbaca24f227b4b7bc51ee5c447af5834f3797
나갱 <helen030816@naver.com>
2025-05-19 16:57:22 +0900

    [feat] 루틴 등록 뷰 각 스크린 구현 (컴포넌트 배치)

--- app/src/main/java/com/example/myapplication/presentation/home/routine/RoutineRepeatScreen.kt
+++ app/src/main/java/com/example/myapplication/presentation/home/routine/RoutineRepeatScreen.kt
@@ -1,8 +1,21 @@
 package com.example.myapplication.presentation.home.routine
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import com.example.myapplication.presentation.component.RepeatCycle
+import com.example.myapplication.presentation.component.RepeatCycleSelector
 
 @Composable
-fun RoutineRepeatScreen() {
-    //TODO 루틴 반복 뷰
+fun RoutineRepeatScreen(
+    selected: RepeatCycle,
+    onSelect: (RepeatCycle) -> Unit,
+    onLeftClick: () -> Unit,
+    onConfirmClick: () -> Unit
+) {
+    RepeatCycleSelector(
+        selected = selected,
+        onSelect = onSelect,
+        onLeftClick = onLeftClick,
+        onConfirmClick = onConfirmClick
+    )
 }
+
