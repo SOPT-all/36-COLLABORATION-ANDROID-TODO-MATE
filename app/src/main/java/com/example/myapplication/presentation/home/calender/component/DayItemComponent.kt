906878e30f6126fe265d70ddfe7ca79202028b49
Daljyeong <jiyoungm1022@gmail.com>
2025-05-20 21:56:00 +0900

    [UI] #13 DayItem 컴포넌트 리플 효과 제거

--- app/src/main/java/com/example/myapplication/presentation/home/calender/component/DayItemComponent.kt
+++ app/src/main/java/com/example/myapplication/presentation/home/calender/component/DayItemComponent.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.presentation.home.calender.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -67,7 +69,10 @@ fun DayItemComponent(
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.clickable { onClick() }
+        modifier = Modifier.clickable(
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() }
+        ) { onClick() }
     ) {
         Text(
             text = dayText.day,
